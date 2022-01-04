778e9bc41bb18703c81c52177a222666ba109457
oderofrancis <francisodero10@gmail.com>
2022-01-04 21:27:07 +0300

    update hosting

--- update/settings.py
+++ update/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'update.urls'
@@ -118,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = Path(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
